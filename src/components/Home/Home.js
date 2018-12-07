7cf4de9a396c0f2c7d89a71596dd86cfa594bcbc
Winnie Ho <winnie-ho@hotmail.co.uk>
2018-12-07 23:45:54 +0000

    Change name.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,7 +6,7 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <h1>Home Page</h1>
+        <h1>Github Search</h1>
         <SearchBar />
         <ResultsList />
       </div>
