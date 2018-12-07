be62c48d72fac3ca7a84f48c5e804fd8785e5e71
Winnie Ho <winnie-ho@hotmail.co.uk>
2018-12-07 20:24:30 +0000

    Adding in the searchBar.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import SearchBar from '../searchBar/SearchBar';
 
 class Home extends Component {
   render() {
     return (
       <div>
         <h1>Home Page</h1>
+        <SearchBar />
       </div>
     )
   }
