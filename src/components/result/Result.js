8729cf4afd28ffd151dec181b26b9cc6cb0e245b
Winnie Ho <winnie-ho@hotmail.co.uk>
2018-12-07 22:56:27 +0000

    Rendering details to the result and html link. Using moment for date.

--- src/components/result/Result.js
+++ src/components/result/Result.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import moment from 'moment';
 
 const Result = ({ result }) => {
   return (
     <div>
-      <h4>{ result.id }</h4>
+      <a href={ result.html_url } target='_blank'><h4>{ result.name }</h4></a>
+      <h6>{ result.owner.login }</h6>
+      <h6>Stargazers: { result.stargazers_count }</h6>
+      <h6>Last updated: { moment(new Date(result.updated_at)).calendar() }</h6>
     </div>
   )
 }
