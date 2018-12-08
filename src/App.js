fea010ea5b3a2439b36339b3cdd761175b4d983d
Winnie Ho <winnie-ho@hotmail.co.uk>
2018-12-08 18:40:01 +0000

    Adding github logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Home from './components/Home/Home';
+import './App.css';
 
 class App extends Component {
   render() {
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import SearchBar from '../searchBar/SearchBar';
 import ResultsList from '../resultsList/ResultsList';
+import './Home.css';
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <h1>Github Search</h1>
+        <div className='row'>
+          <img alt='github logo' src='/assets/github-logo.png'/>
+          <h1>Github Search</h1>
+        </div>
         <SearchBar />
         <ResultsList />
       </div>
