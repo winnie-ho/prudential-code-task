f49897afebb162d5b34f7d5f490f521a00cb2217
Winnie Ho <winnie-ho@hotmail.co.uk>
2018-12-09 16:29:16 +0000

    Renaming folder.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Home from './components/Home/Home';
+import Home from './components/home/Home';
 import './App.css';
 
 class App extends Component {
